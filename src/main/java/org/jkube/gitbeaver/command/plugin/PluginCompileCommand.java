8625698773b12d3178a8cf440f4eaf8dedba34f2
Jan Kneissler <jan.kneissler@breuninger.de>
2023-04-25 21:20:50 +0200

    compile plugin variant with jar files path

--- src/main/java/org/jkube/gitbeaver/command/plugin/PluginCompileCommand.java
+++ src/main/java/org/jkube/gitbeaver/command/plugin/PluginCompileCommand.java
@@ -1,5 +1,6 @@
 package org.jkube.gitbeaver.command.plugin;
 
+import org.jkube.gitbeaver.AbstractCommand;
 import org.jkube.gitbeaver.SimpleCommand;
 import org.jkube.gitbeaver.WorkSpace;
 import org.jkube.gitbeaver.plugin.PluginCompiler;
@@ -14,27 +15,34 @@ import java.util.Map;
 /**
  * Usage: plugin compile soureDir
  */
-public class PluginCompileCommand extends SimpleCommand {
+public class PluginCompileCommand extends AbstractCommand {
 
-    private static final String PLUGIN_DIRECTORY = "directory";
-    private static final String LIBRARIES_FOLDER = "lib";
+    private static final String SOURCES_DIRECTORY = "sources";
+    private static final String JAR_DIRECTORY = "jars";
     private static final String JAR_EXTENSION = ".jar";
 
     public PluginCompileCommand() {
-        super("PLUGIN COMPILE", """
+        super("""
                This command compiles the source files of a plugin. The java sources are supposed to reside in a
                directory within the workspace. The compiled class files are written to the class path.
             """);
-        argument(PLUGIN_DIRECTORY, """
+        commandlineVariant("PLUGIN COMPILE "+SOURCES_DIRECTORY, "Compile without libraries");
+        commandlineVariant("PLUGIN COMPILE "+SOURCES_DIRECTORY+" "+JAR_DIRECTORY, "Compile sources using libraries in jar files");
+        argument(SOURCES_DIRECTORY, """
                 The directory (relative to workspace path) in which the plugin source files are located.
+            """);
+        argument(JAR_DIRECTORY, """
+                The directory (relative to workspace path) in which the library jar files files are located.
             """);
     }
 
     @Override
-    protected void execute(WorkSpace workSpace, Map<String, String> arguments) {
-        Path sourcePath = workSpace.getAbsolutePath(arguments.get(PLUGIN_DIRECTORY));
-        File[] jarFiles = sourcePath
-                .resolve(LIBRARIES_FOLDER)
+    public void execute(Map<String, String> variables, WorkSpace workSpace, Map<String, String> arguments) {
+        Path sourcePath = workSpace.getAbsolutePath(arguments.get(SOURCES_DIRECTORY));
+        Path jarPath = workSpace.getAbsolutePath(arguments.get(JAR_DIRECTORY));
+        File[] jarFiles = jarPath == null ? null :
+                sourcePath
+                .resolve(jarPath)
                 .toFile()
                 .listFiles((d,fn) -> fn .endsWith(JAR_EXTENSION));
         new PluginCompiler(Environment.classPath(), jarFiles == null ? Collections.emptyList() : List.of(jarFiles)).compile(sourcePath);
